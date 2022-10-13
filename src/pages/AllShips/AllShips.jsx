c116e85d25eceefda7418048a71a551f2f1190a0
mrunlockedtech <jrbhrn@gmail.com>
2022-10-13 11:36:55 -0500

    Add Page Numbers

--- src/pages/AllShips/AllShips.jsx
+++ src/pages/AllShips/AllShips.jsx
@@ -17,27 +17,39 @@ const AllShips = () => {
   }, [pageNumber])
 
   const handlePrevButton = () => {
-      setPageNumber(pageNumber-1)
+    if(pageNumber > 1)
+    setPageNumber(pageNumber - 1)
   }
   const handleNextButton = () => {
-    setPageNumber(pageNumber+1)
+    if(pageNumber < Math.ceil(shipData.count/10))
+    setPageNumber(pageNumber + 1)
   }
 
   return (
     <>
-      <div className="nav-buttons">
-        <button onClick={handlePrevButton} disabled={!shipData.previous}>Prev</button>
-        <button onClick={handleNextButton} disabled={!shipData.next}>Next</button>
-      </div>
-      <div className="icon-container">
-        {starShips.map(starship =>
-          <Link to='/starship' state={{ starship }} key={starship.name}>
-            <div className="class-div">
-              {starship.name}
-            </div>
-          </Link>
-        )}
-      </div>
+      {starShips.length ? <>
+        <div className="nav-buttons">
+          <button onClick={handlePrevButton} disabled={!shipData.previous}>Prev</button>
+          <button onClick={handleNextButton} disabled={!shipData.next}>Next</button>
+        </div>
+        <div className="nav-buttons">
+        <p>Page: {pageNumber} of {Math.ceil(shipData.count/10)}</p>
+        </div>
+        <div className="icon-container">
+          {starShips.map(starship =>
+            <Link to='/starship' state={{ starship }} key={starship.name}>
+              <div className="class-div">
+                {starship.name}
+              </div>
+            </Link>
+          )}
+        </div>
+      </>
+        :
+        <div className="loading-text">Loading...</div>
+      }
+
+
     </>
   );
 }
--- src/pages/ShipDetails/ShipDetails.jsx
+++ src/pages/ShipDetails/ShipDetails.jsx
@@ -17,12 +17,14 @@ const ShipDetails = () => {
 
   return (
     <>
-      <div className="ship-div">
-        <p>NAME: {shipDetails.name}</p>
-        <p>MODEL: {shipDetails.model}</p>
-        <Link to={'/'}><p>RETURN</p></Link>
-      </div>
-
+    {shipDetails.model ? 
+          <div className="ship-div">
+          <p>NAME: {shipDetails.name}</p>
+          <p>MODEL: {shipDetails.model}</p>
+          <Link to={'/'}><p>RETURN</p></Link>
+        </div> :
+        <div className="loading-text">Loading...</div>
+  }
 
     </>
   );
