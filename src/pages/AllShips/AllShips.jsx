c257787c960f002a31bbda70bdd2266ae3dcf5ac
mrunlockedtech <jrbhrn@gmail.com>
2022-10-13 11:00:16 -0500

    Implement Pagination Buttons

--- src/pages/AllShips/AllShips.jsx
+++ src/pages/AllShips/AllShips.jsx
@@ -4,21 +4,30 @@ import { Link } from "react-router-dom";
 
 const AllShips = () => {
   const [starShips, setStarShips] = useState([])
-  const [pageNumber, setPageNumber] = useState()
+  const [pageNumber, setPageNumber] = useState(1)
+  const [shipData, setShipData] = useState({})
 
   useEffect(() => {
     const fetchShipData = async () => {
-      const shipData = await getAllStarships()
+      const shipData = await getAllStarships(pageNumber)
+      setShipData(shipData)
       setStarShips(shipData.results)
     }
     fetchShipData()
-  }, [])
+  }, [pageNumber])
+
+  const handlePrevButton = () => {
+      setPageNumber(pageNumber-1)
+  }
+  const handleNextButton = () => {
+    setPageNumber(pageNumber+1)
+  }
 
   return (
     <>
       <div className="nav-buttons">
-        <button>Prev</button>
-        <button>Next</button>
+        <button onClick={handlePrevButton} disabled={!shipData.previous}>Prev</button>
+        <button onClick={handleNextButton} disabled={!shipData.next}>Next</button>
       </div>
       <div className="icon-container">
         {starShips.map(starship =>
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -1,13 +1,13 @@
 const baseUrl = 'https://swapi.dev/api/'
 
-// export async function getAllStarships(pageNumber){
-//   const res = await fetch(`${baseUrl}starships/?page=${pageNumber}`)
-//   return res.json()
-// }
-export async function getAllStarships(){
-  const res = await fetch(`${baseUrl}starships/`)
+export async function getAllStarships(pageNumber){
+  const res = await fetch(`${baseUrl}starships/?page=${pageNumber}`)
   return res.json()
 }
+// export async function getAllStarships(){
+//   const res = await fetch(`${baseUrl}starships/`)
+//   return res.json()
+// }
 
 export async function getDetails(appUrl) {
   const res = await fetch(appUrl)
